fe24b1ba8e3d1a625ef616beba6ec0a682338471
Ania Leśniak <lesniakania@gmail.com>
2010-08-13 10:53:46 +0200

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memorizr::Application.config.secret_token = '930367d4ae2b47434987cf12fb2e97cdbf48ac705943f44a1025887fb56052ce28dcc9de69aeb8c4b50b55e05ee7c85474d0dda1b9bf6caf30f00bdf19066d37'
