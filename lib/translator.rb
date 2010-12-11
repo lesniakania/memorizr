ec54d00df2fbae129cedc09ac6514692f84bab3c
Ania <lesniakania@gmail.com>
2010-12-12 00:18:38 +0100

    wip

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,9 @@ require 'uri'
 require 'nokogiri'
 
 class Translator
+  BASE_URL = "https://www.googleapis.com/language/translate/v2"
+  API_KEY = "AIzaSyAkQ54KDmJI8xsU8OP4C60EiJTgieXSSto"
+
   def self.extract_meanings(word, from, to)
     url = build_url(word.value, from.value, to.value)
     response = Net::HTTP.get_response(url)
@@ -21,5 +24,21 @@ class Translator
 
   def self.build_url(word, from, to)
     URI.parse("http://translate.google.pl/?hl=en&sl=#{from}&tl=#{to}&q=#{URI.encode(word)}")
+  end
+
+  def self.extract_meanings_from_api
+    uri = build_url(word.value, from.value, to.value)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    data = JSON.parse(response.body)
+    results = [data['data']['translations'].first['translatedText']]
+  end
+
+  def self.build_api_uri(word, from, to)
+    URI.parse("#{BASE_URL}?key=#{API_KEY}&source=#{from}&target=#{to}&q=#{URI.encode(word)}")
   end
 end
